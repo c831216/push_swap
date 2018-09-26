32b11ab55986ffe94b1bce59bb71e90b36d88b90
User <user@Mac.local>
2018-09-26 09:51:46 +0200

    taday

--- PUSH_SWAP/functions.c
+++ PUSH_SWAP/functions.c
@@ -44,12 +44,15 @@ void dup_check(t_all *all) /*2 I now need to take my stored stack and check for 
         all->b = 0;
         while (all->stack_a[all->i][all->b])
         {
-          if (!ft_isdigit(all->stack_a[all->i][all->b]))
+          if (all->stack_a[all->i][all->b] == '-' || all->stack_a[all->i][all->b] == '+')
+            all->b++;
+          else if (!ft_isdigit(all->stack_a[all->i][all->b]))
           {
             ft_putstr_fd("Error2\n", 2);
             exit(0);
           }
-          all->b++;
+          else
+            all->b++;
         }
         all->count++;
         if (all->count > 1)
@@ -67,19 +70,23 @@ void dup_check(t_all *all) /*2 I now need to take my stored stack and check for 
 void make_int_array(t_all *all) /*3 I now need to convert to int array */
 {
   all->i = 0;
+  long tmp = 0;
   all->int_stack_a = (int *)malloc(sizeof(int) * all->m);
   all->a_counter = 0;
   while (all->i < all->m)
   {
-    all->int_stack_a[all->i] = ft_atoi(all->stack_a[all->i]);
-    if ((int)(all->int_stack_a[all->i]) > -2147483647 && (int)(all->int_stack_a[all->i]) < 2147483647)
+    tmp = ft_atoi(all->stack_a[all->i]);
+    if (tmp <= 2147483647 && tmp >= -2147483647)
     {
-      all->i++;
-      all->a_counter++;
+      all->int_stack_a[all->i] = ft_atoi(all->stack_a[all->i]);
+      {
+        all->i++;
+        all->a_counter++;
+      }
     }
     else
     {
-      ft_putstr_fd("Error\n", 2);
+      ft_putstr_fd("MAXINT - Error\n", 2);
       exit(0);
     }
   }
